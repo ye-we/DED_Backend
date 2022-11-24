68644ec00513d865241a7b0de11c26447e3ed3f2
yewe <khalid.yewe@gmail.com>
2022-11-24 14:16:47 +0300

    updated mongoose connect options

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = require('./app');
 mongoose.connect(
   process.env.DATABASE,
   // process.env.HOST,
-  { useNewUrlParser: true },
+  { useNewUrlParser: true, bufferCommands: false },
   err => {
     if (err) {
       console.log(err);
